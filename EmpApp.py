7ca14c8152c826017aa8888f169907c4a4f33fe2
TEEBOONCHONG <77604145+TEEBOONCHONG@users.noreply.github.com>
2022-09-27 11:32:55 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -1,181 +1,231 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <!-- basic -->
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <!-- mobile metas -->
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-      <!-- site metas -->
-      <title>Add Employee Details</title>
-      <meta name="keywords" content="">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <!-- bootstrap css -->
-      <link rel="stylesheet" href="css/bootstrap.min.css">
-      <!-- style css -->
-      <link rel="stylesheet" href="css/style.css">
-      <!-- Responsive-->
-      <link rel="stylesheet" href="css/responsive.css">
-      <!-- fevicon -->
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
-      <!-- Scrollbar Custom CSS -->
-      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-      <!-- Tweaks for older IEs-->
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-      <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-      <link rel="stylesheet" href="css/employeeLeave.css">
-      <link rel="stylesheet" href="css/payroll.css">
-   </head>
+from flask import Flask, render_template, request
+from pymysql import connections
+import os
+import boto3
+from config import *
+
+app = Flask(__name__)
+
+bucket = custombucket
+region = customregion
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'employee'
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('AddEmp.html')
+
+@app.route("/find", methods=['GET', 'POST'])
+def findEmp():
+    return render_template('GetEmp.html')
+
+@app.route("/empLeave", methods=['GET', 'POST'])
+def leave():
+    return render_template('leave.html')
+
+@app.route("/empPayroll", methods=['GET', 'POST'])
+def empPayroll():
+    return render_template('payroll.html')
+
+
+@app.route("/about", methods=['POST'])
+def about():
+    return render_template('www.intellipaat.com')
+
+
+@app.route("/addemp", methods=['POST'])
+def AddEmp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file']
+    salary = request.form['salary']
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, salary))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('AddEmpOutput.html', name=emp_name)
 
 
 
 
-   <!-- body -->
-   <body class="main-layout">
-      <!-- loader  -->
-      <div class="loader_bg">
-         <!-- <div class="loader"><img src="images/loading.gif" alt="#" /></div> -->
-      </div>
-      <!-- end loader -->
-      <div id="mySidepanel" class="sidepanel">
-         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-         <form action="/addemp" autocomplete="on" method = "POST" enctype="multipart/form-data">
-            <a href="index.html">Home </a>
-         <a href="about.html">About</a>
-         <button type="submit" style="background: black; height: 45px; width: 200px; color:white; font:oblique;" formaction="http://3.134.115.108:80/">Add Employee</button>
-         <button type="submit" style="background: black; height: 45px; width: 200px; color:white; font:oblique;" formaction="http://3.134.115.108:80/find">Find Employee</button>
-         <button type="submit" style="background: black; height: 45px; width: 200px; color:white; font:oblique;" formaction="http://3.134.115.108:80/empLeave">Apply Leave</button>
-         <button type="submit" style="background: black; height: 45px; width: 200px; color:white; font:oblique;" formaction="http://3.134.115.108:80/empPayroll">Payroll</button>
-         </form>
-      </div>
+@app.route("/fetchdata", methods=['GET'])
+def GetEmp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file']
+    salary = request.form['salary']
 
-      <!-- header -->
-      <header>
-         <!-- header inner -->
-         <div class="header">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-md-4 col-sm-4">
-                     <div class="logo">
-                        <!-- <a href="index.html"><img src="images/logo.png" alt="#" /></a> -->
-                     </div>
-                  </div>
-                  <div class="col-md-8 col-sm-8">
-                     <div class="right_bottun">
-                        <ul class="conat_info d_none ">
-                           <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-                           <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-                        </ul>
-                        <button class="openbtn" onclick="openNav()"><img src="images/menu_icon.png" alt="#"/> </button> 
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </header>
-      <!-- end header inner -->
-      <!-- end header -->
-      
-      <!-- banner -->
-      <section class="banner_main">
-         <center>
-            <font color="black" size="4" style="font-family: avenir">
-         
-               <h1 style="color: DodgerBlue">Employee Database</h1>
-        <form action="/addemp" autocomplete="on" method = "POST" enctype="multipart/form-data">
+    select_sql = "SELECT (%s, %s, %s, %s, %s, %s) FROM employee WHERE emp_id=emp_id"
+    cursor = db_conn.cursor()
 
-            <button type="submit" formaction="/getemp" style="background: grey; height: 45px; width: 300px; color:white; font:oblique;">GET EMPLOYEE INFORMATION</button><br><br>
+    if emp_image_file.filename == "":
+        return "Please select a file"
 
-              Employee ID:<br> <input style="height:25px;font-size:14pt; color:grey;" type="number" name="emp_id" autofocus size="40"><br><br>
+    try:
 
-              First Name:<br> <input style="height:25px;font-size:14pt;color:grey;" type="text" name="first_name" ><br><br>
+        cursor.execute(select_sql, (emp_id, first_name, last_name, pri_skill, location, salary))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        #emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"#
+        s3 = boto3.resource('s3')
 
-              Last Name:<br> <input style="height:25px;font-size:14pt;color:grey;" type="text" name="last_name"><br><br>
+        try:
+            print("Data selected from MySQL RDS...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
 
-              Primary Skills:<br> <input style="height:25px;font-size:14pt;color:grey;" type="text" name="pri_skill"><br><br>
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
 
-              Location:<br> <input style="height:25px;font-size:14pt;color:grey;" type="text" name="location"><br><br>
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
 
-              Salary:<br> <input style="height:25px;font-size:14pt;color:grey;" type="text" name="salary"><br><br>
+        except Exception as e:
+            return str(e)
 
-              Image: <input type=file name="emp_image_file" style="height:40px;font-size:14pt;color:grey;"> <br><br>
+    finally:
+        cursor.close()
 
-              <button type="submit" style="background: grey; height: 45px; width: 200px; color: white; size: 5; font:oblique;">UPDATE DATABASE</button>
-
-        </form>
-      </font>
-   </center>
-      </section>
-      <!-- end banner -->
-
-      
+    print("all modification done...")
+    return render_template('GetEmpOutput.html', id=emp_id, fname=first_name, lname=last_name, interest=pri_skill, location=location, image_url=object_url, salary=salary)
 
 
 
 
-      <!--  footer -->
-      <footer>
-         <div class="footer bottom_cross1">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-4">
-                     <ul class="location_icon">
-                        <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a> Address : Lorem Ipsum <br> is simply dummy 
-                        </li>
-                        <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>Phone :  +(1234) 567 890</li>
-                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>Email : demo@gmail.com</li>
-                     </ul>
-                     <form class="bottom_form">
-                        <h3>Newsletter</h3>
-                        <input class="enter" placeholder="Enter your email" type="text" name="Enter your email">
-                        <button class="sub_btn">subscribe</button>
-                     </form>
-                  </div>
-                  <div class="col-md-8">
-                     <div class="map">
-                        <figure><img src="images/map.png" alt="#"/></figure>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="copyright">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-md-12">
-                        <p>© 2019 All Rights Reserved. Design by<a href="https://html.design/"> Free Html Templates</a></p>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
-      <!-- end footer -->
+
+@app.route("/applyleave", methods=['GET', 'POST'])
+def ApplyLeave():
+    emp_id = request.form['emp_id']
+    date_leave = request.form['date_leave']
+    reason_leave = request.form['reason_leave']
+    support_doc_file = request.form['support_doc_file']
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s) WHERE emp_id=emp_id"
+    cursor = db_conn.cursor()
+
+    if support_doc_file.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (date_leave, reason_leave))
+        db_conn.commit()
+        # Uplaod image file in S3 #
+        support_doc_file_in_s3 = "emp-id-" + str(emp_id) + "_support_doc_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=support_doc_file_in_s3, Body=support_doc_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                support_doc_file_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('GetLeaveOutput.html', id=emp_id, date=date_leave, reason=reason_leave, docs_url=object_url)
+
+
+@app.route("/payroll", methods=['GET', 'POST'])
+def Payroll():
+    emp_id = request.form['emp_id']
+    salary = request.form['salary']
+    deduct = request.form['deduct']
+
+    select_sql = "SELECT (%s) FROM employee WHERE emp_id=emp_id"
+    insert_sql = "INSERT INTO employee VALUES (%s) WHERE emp_id=emp_id"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (salary))
+        cursor.execute(insert_sql, (deduct))
+        db_conn.commit()
+        new_salary = salary - deduct
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('GetPayrollOutput.html', id=emp_id, salary=salary, deduct=deduct, new_salary=new_salary)
 
 
 
-
-      
-      <!-- Javascript files-->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
-      <script>
-         function openNav() {
-           document.getElementById("mySidepanel").style.width = "250px";
-         }
-         
-         function closeNav() {
-           document.getElementById("mySidepanel").style.width = "0";
-         }
-      </script>
-   </body>
-</html>
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
